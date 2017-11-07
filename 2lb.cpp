e42cdf4d90b620a18cb7a7b9e174d68c1cd60ba9
beatusapril <33068962+beatusapril@users.noreply.github.com>
2017-11-07 19:05:21 +0300

    Update 2lb.cpp

--- 2lb.cpp
+++ 2lb.cpp
@@ -1,7 +1,7 @@
-/*21.Реализовать класс Cursor. Полями являются координаты курсора по горизонтали и
-вертикали – целые положительные числа, вид курсора – горизонтальный или
-вертикальный, размер курсора - целое от 1 до 15. Реализовать методы изменения вида
-курсора, изменения размера курсора, метод гашения и восстановления курсора. Воищева З.Ю.*/
+/*21.Р РµР°Р»РёР·РѕРІР°С‚СЊ РєР»Р°СЃСЃ Cursor. РџРѕР»СЏРјРё СЏРІР»СЏСЋС‚СЃСЏ РєРѕРѕСЂРґРёРЅР°С‚С‹ РєСѓСЂСЃРѕСЂР° РїРѕ РіРѕСЂРёР·РѕРЅС‚Р°Р»Рё Рё
+РІРµСЂС‚РёРєР°Р»Рё вЂ“ С†РµР»С‹Рµ РїРѕР»РѕР¶РёС‚РµР»СЊРЅС‹Рµ С‡РёСЃР»Р°, РІРёРґ РєСѓСЂСЃРѕСЂР° вЂ“ РіРѕСЂРёР·РѕРЅС‚Р°Р»СЊРЅС‹Р№ РёР»Рё
+РІРµСЂС‚РёРєР°Р»СЊРЅС‹Р№, СЂР°Р·РјРµСЂ РєСѓСЂСЃРѕСЂР° - С†РµР»РѕРµ РѕС‚ 1 РґРѕ 15. Р РµР°Р»РёР·РѕРІР°С‚СЊ РјРµС‚РѕРґС‹ РёР·РјРµРЅРµРЅРёСЏ РІРёРґР°
+РєСѓСЂСЃРѕСЂР°, РёР·РјРµРЅРµРЅРёСЏ СЂР°Р·РјРµСЂР° РєСѓСЂСЃРѕСЂР°, РјРµС‚РѕРґ РіР°С€РµРЅРёСЏ Рё РІРѕСЃСЃС‚Р°РЅРѕРІР»РµРЅРёСЏ РєСѓСЂСЃРѕСЂР°. Р’РѕРёС‰РµРІР° Р—.Р®.*/
 //#include "stdafx.h"
 #include <iostream>
 #include <Windows.h>
@@ -9,7 +9,7 @@
 #include <sstream>
 using namespace std;
 
-enum Cursor_Type		// тип курсора
+enum Cursor_Type		// С‚РёРї РєСѓСЂСЃРѕСЂР°
 {
 	CursorType_Vert,
 	CursorType_Horz,
@@ -18,10 +18,10 @@ enum Cursor_Type		// тип курсора
 class Cursor
 {
 private:
-		unsigned int m_X; // координата курсора по горизонтали
-		unsigned int m_Y; // координата курсора по вертикали
-		Cursor_Type m_Type; // тип курсора
-		short m_Size;      // размер курсора 1-15
+		unsigned int m_X; // РєРѕРѕСЂРґРёРЅР°С‚Р° РєСѓСЂСЃРѕСЂР° РїРѕ РіРѕСЂРёР·РѕРЅС‚Р°Р»Рё
+		unsigned int m_Y; // РєРѕРѕСЂРґРёРЅР°С‚Р° РєСѓСЂСЃРѕСЂР° РїРѕ РІРµСЂС‚РёРєР°Р»Рё
+		Cursor_Type m_Type; // С‚РёРї РєСѓСЂСЃРѕСЂР°
+		short m_Size;      // СЂР°Р·РјРµСЂ РєСѓСЂСЃРѕСЂР° 1-15
 
 		friend bool operator==(const Cursor& a, const Cursor& b)
 		{
@@ -35,49 +35,49 @@ public:
 		{
 		}
 
-		void SetPos(unsigned int x, unsigned int y) throw() // изменение позиции курсора
+		void SetPos(unsigned int x, unsigned int y) throw() // РёР·РјРµРЅРµРЅРёРµ РїРѕР·РёС†РёРё РєСѓСЂСЃРѕСЂР°
 		{
 			m_X = x;
 			m_Y = y;
 		}
 
-		void SetType(Cursor_Type t) throw()		// изменение типа курсора
+		void SetType(Cursor_Type t) throw()		// РёР·РјРµРЅРµРЅРёРµ С‚РёРїР° РєСѓСЂСЃРѕСЂР°
 		{
 			m_Type = t;
 		}
 
-		void SetSize(short s) throw()	// изменение размера курсора
+		void SetSize(short s) throw()	// РёР·РјРµРЅРµРЅРёРµ СЂР°Р·РјРµСЂР° РєСѓСЂСЃРѕСЂР°
 		{
 			m_Size = s;
 		}
 
 
 
-		void Init(unsigned int x, unsigned int y, Cursor_Type m_Tp, short msz) // инициализация курсора
+		void Init(unsigned int x, unsigned int y, Cursor_Type m_Tp, short msz) // РёРЅРёС†РёР°Р»РёР·Р°С†РёСЏ РєСѓСЂСЃРѕСЂР°
 		{
 			SetPos(x, y);
 			SetType(m_Tp);
 			SetSize(msz);
 		}
 
-		void Display()							// вывод курсора на экране
+		void Display()							// РІС‹РІРѕРґ РєСѓСЂСЃРѕСЂР° РЅР° СЌРєСЂР°РЅРµ
 		{
 			cout << "[x] = " << m_X << endl;
 			cout << "[y] = " << m_Y << endl;
-			cout << "Вид курсора - ";
+			cout << "Р’РёРґ РєСѓСЂСЃРѕСЂР° - ";
 			switch (m_Type)
 			{
 			case CursorType_Vert:
-				cout << "вертикальный" << endl;
+				cout << "РІРµСЂС‚РёРєР°Р»СЊРЅС‹Р№" << endl;
 				break;
 			case CursorType_Horz:
-				cout << "горизонтадьный" << endl;
+				cout << "РіРѕСЂРёР·РѕРЅС‚Р°РґСЊРЅС‹Р№" << endl;
 				break;
 			}
-			cout << "Размер курсора - " << m_Size << endl;
+			cout << "Р Р°Р·РјРµСЂ РєСѓСЂСЃРѕСЂР° - " << m_Size << endl;
 		}
 
-		void Read()								// ввод данных о курсоре
+		void Read()								// РІРІРѕРґ РґР°РЅРЅС‹С… Рѕ РєСѓСЂСЃРѕСЂРµ
 		{
 			unsigned int x, y;
 			short size;
@@ -94,32 +94,32 @@ public:
 			} while (y < 0);
 
 			do{
-				cout << "\nВыберите вид курсора: 1 - горизонтальный 2 - вертикальный\n";
+				cout << "\nР’С‹Р±РµСЂРёС‚Рµ РІРёРґ РєСѓСЂСЃРѕСЂР°: 1 - РіРѕСЂРёР·РѕРЅС‚Р°Р»СЊРЅС‹Р№ 2 - РІРµСЂС‚РёРєР°Р»СЊРЅС‹Р№\n";
 				cin >> k;
 			} while (k != 1 && k != 2);
 
 			do{
-				cout << "\nВведите размер курсора\n";
+				cout << "\nР’РІРµРґРёС‚Рµ СЂР°Р·РјРµСЂ РєСѓСЂСЃРѕСЂР°\n";
 				cin >> size;
 			} while (size<0 || size >15);
 			Init(x, y, static_cast<Cursor_Type>(k), size);
 		}
 
-		string StringToInt(unsigned int x)   // конвертирование в строку
+		string StringToInt(unsigned int x)   // РєРѕРЅРІРµСЂС‚РёСЂРѕРІР°РЅРёРµ РІ СЃС‚СЂРѕРєСѓ
 		{
 			std::ostringstream oss;
 			oss << x;
 			return oss.str();
 		}
 
-		string StringToShort(short sz)		// конвертирование в строку
+		string StringToShort(short sz)		// РєРѕРЅРІРµСЂС‚РёСЂРѕРІР°РЅРёРµ РІ СЃС‚СЂРѕРєСѓ
 		{
 			std::ostringstream oss;
 			oss << sz;
 			return oss.str();
 		}
 
-		void ChangePos()            // изменение позиции курсора
+		void ChangePos()            // РёР·РјРµРЅРµРЅРёРµ РїРѕР·РёС†РёРё РєСѓСЂСЃРѕСЂР°
 		{
 			int x, y;
 			do{
@@ -134,11 +134,11 @@ public:
 			SetPos(x, y);
 		}
 
-		void ChangeTypeCursor()			// изменение типа курсора
+		void ChangeTypeCursor()			// РёР·РјРµРЅРµРЅРёРµ С‚РёРїР° РєСѓСЂСЃРѕСЂР°
 		{
 			int k;
 			do{
-				cout << "\nВыберите вид курсора: 1 - горизонтальный 2 - вертикальный\n";
+				cout << "\nР’С‹Р±РµСЂРёС‚Рµ РІРёРґ РєСѓСЂСЃРѕСЂР°: 1 - РіРѕСЂРёР·РѕРЅС‚Р°Р»СЊРЅС‹Р№ 2 - РІРµСЂС‚РёРєР°Р»СЊРЅС‹Р№\n";
 				cin >> k;
 			} while (k != 1 && k != 2);
 
@@ -153,24 +153,24 @@ public:
 			}
 		}
 
-		void ChangeSize()					// изменение размера курсора
+		void ChangeSize()					// РёР·РјРµРЅРµРЅРёРµ СЂР°Р·РјРµСЂР° РєСѓСЂСЃРѕСЂР°
 		{
 			short size;
 			do{
-				cout << "\nВведите размер курсора\n";
+				cout << "\nР’РІРµРґРёС‚Рµ СЂР°Р·РјРµСЂ РєСѓСЂСЃРѕСЂР°\n";
 				cin >> size;
 			} while (size<0 || size >15);
 			SetSize(size);
 		}
 
-		void DampingCursor()	// гашение курсора
+		void DampingCursor()	// РіР°С€РµРЅРёРµ РєСѓСЂСЃРѕСЂР°
 		{
 			while (m_Size >= 0){
 				m_Size--;
 			}
 		}
 
-		void RecoveryCursor()	//восстновление курсоа
+		void RecoveryCursor()	//РІРѕСЃСЃС‚РЅРѕРІР»РµРЅРёРµ РєСѓСЂСЃРѕР°
 		{
 			while (m_Size < 0){
 				m_Size++;
@@ -192,16 +192,16 @@ int main()
 
 	for (;;)
 	{
-		cout << " Меню  " << endl;
-		cout << "1 - Ввод данных" << endl;
-		cout << "2 - Вывод данных" << endl;
-		cout << "3 - Изменение положения курсора" << endl;
-		cout << "4 - Изменение типа курсора" << endl;
-		cout << "5 - Изменение размера курсора" << endl;
-		cout << "6 - Погашение курсора" << endl;
-		cout << "7 - Восстановление курсора" << endl;
-		cout << "8 - Выход " << endl << endl;
-		cout << "Введите ваш вариант " << endl;
+		cout << " РњРµРЅСЋ  " << endl;
+		cout << "1 - Р’РІРѕРґ РґР°РЅРЅС‹С…" << endl;
+		cout << "2 - Р’С‹РІРѕРґ РґР°РЅРЅС‹С…" << endl;
+		cout << "3 - РР·РјРµРЅРµРЅРёРµ РїРѕР»РѕР¶РµРЅРёСЏ РєСѓСЂСЃРѕСЂР°" << endl;
+		cout << "4 - РР·РјРµРЅРµРЅРёРµ С‚РёРїР° РєСѓСЂСЃРѕСЂР°" << endl;
+		cout << "5 - РР·РјРµРЅРµРЅРёРµ СЂР°Р·РјРµСЂР° РєСѓСЂСЃРѕСЂР°" << endl;
+		cout << "6 - РџРѕРіР°С€РµРЅРёРµ РєСѓСЂСЃРѕСЂР°" << endl;
+		cout << "7 - Р’РѕСЃСЃС‚Р°РЅРѕРІР»РµРЅРёРµ РєСѓСЂСЃРѕСЂР°" << endl;
+		cout << "8 - Р’С‹С…РѕРґ " << endl << endl;
+		cout << "Р’РІРµРґРёС‚Рµ РІР°С€ РІР°СЂРёР°РЅС‚ " << endl;
 		cin >> number;
 
 		switch (number)
@@ -221,10 +221,10 @@ int main()
 		case 7:
 			cr.RecoveryCursor(); break;
 		case 8:
-			cout << "Выход " << endl; exit(1);
+			cout << "Р’С‹С…РѕРґ " << endl; exit(1);
 			break;
 		default:
-			cout << "Некорретный ввод " << endl;
+			cout << "РќРµРєРѕСЂСЂРµС‚РЅС‹Р№ РІРІРѕРґ " << endl;
 			break;
 		}
 		getchar();
